a2768c60997beaec50f14ca281d57ef6c6c2704f
Leonan Ferrnando Orth <lo0112437@>
2021-11-26 21:54:20 -0300

    React atualizado até demais

--- src/components/LendingPageComponents/SelectInstallments/Select.jsx
+++ src/components/LendingPageComponents/SelectInstallments/Select.jsx
@@ -5,7 +5,9 @@ import "./styles.css";
 const Select = ({ value }) => {
   var expanded = false;
 
-  const [checked, setChecked] = useState("");
+  const [textLabel, setTextLabel] = useState("");
+  const [hidden, setHidden] = useState(true);
+  const [arrayLenght, setArrayLenght] = useState(0);
 
   const handleClickDropdown = (e) => {
     let icon = document.querySelector(".dropdown-title > i");
@@ -15,7 +17,7 @@ const Select = ({ value }) => {
       icon.classList.add("active");
       dropdownMenu.classList.add("open");
       expanded = true;
-    } else if (expanded || e.target !== dropdownMenu) {
+    } else {
       icon.classList.remove("active");
       dropdownMenu.classList.remove("open");
       expanded = false;
@@ -23,31 +25,52 @@ const Select = ({ value }) => {
   };
 
   const handleInputChecked = (e) => {
+    //console.log("e", e);
     var inputs = document.querySelectorAll("input[type=checkbox]");
-    var inputTarget = document.querySelector("#" + e.target.getAttribute("id"));
-    setChecked("checked");
+    inputs.forEach((element) => {
+      if (element.checked) {
+        setHidden(false);
+        setArrayLenght(e);
+        handleClickDropdown(e);
+      }
+    });
 
-    inputs.forEach((element) => {});
+    setTextLabel("Escolha o número de parcelas");
   };
 
   return (
     <div className="select-container">
-      <div className="dropdownEl" data-dropdown>
-        <div
-          className="dropdown-title"
-          onClick={handleClickDropdown}
-          data-dropdown-button
-        >
-          <span>Escolha o número de parcelas</span>
-          <i className="bi bi-caret-down" />
+      <div className="dropdownEl">
+        <div onClick={() => handleClickDropdown()}>
+          <div className="dropdown-title">
+            <span>Escolha o número de parcelas</span>
+            <i className="bi bi-caret-down" />
+          </div>
+          <div className="dropdown-subtitle" hidden={hidden}>
+            <div>
+              <strong>
+                {" "}
+                {arrayLenght}x de {value / arrayLenght}
+              </strong>
+            </div>
+            <span>
+              Juros de <del>3,95%</del> 3,56% ao mês
+              <span className="text-success">(-10%)</span>
+            </span>
+            <br></br>
+            <span className="text-success">
+              Desconto nos juros personalizados para você
+            </span>
+          </div>
         </div>
+
         <div className="dropdown-menuEl">
           <ul>
             {Array.from({ length: 11 }, (v, k) => k + 2).map((x) => {
               return (
                 <li>
                   <input
-                    onChange={handleInputChecked}
+                    onChange={() => handleInputChecked(x)}
                     type="checkbox"
                     name={`check${x}`}
                     id={`check${x}`}
